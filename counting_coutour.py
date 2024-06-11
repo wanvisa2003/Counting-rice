495f82747e49a000107c8ef9482d6f6d24c7d1af
wansai <wannvisa4488@gmail.com>
2024-06-12 00:57:43 +0700

    kernal and threshold

--- counting_coutour.py
+++ counting_coutour.py
@@ -6,3 +6,12 @@ image_path  = 'rrice.jpg'
 image = cv2.imread(image_path)
 original_image = image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+kernal = np.ones((10, 10), np.uint8)
+thresh = cv2.erode(thresh, kernal, iterations = 1)
+thresh = cv2.dilate(thresh, kernal, iterations = 1)
+kernal = np.ones((5,5), np.uint8)
+thresh =  cv2.erode(thresh, kernal, iterations = 1)
+
+contour, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+sorted_contours = sorted(contour, key=cv2.contourArea, reverse=False)
